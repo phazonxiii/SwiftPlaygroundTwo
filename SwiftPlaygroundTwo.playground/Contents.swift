cb4ace84652b11ee24ce02aed3ee32ad7f3649b5
Cliff Bailey <69611676+phazonxiii@users.noreply.github.com>
2022-02-09 16:16:44 -1000

    Completed Steps 1 & 2
    Moving on to step 3

---
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var goodGrade = "I've been doing all my work"
+if goodGrade == "I've been doing all my work"
+{
+    print("I should be getting an A at this point")
+}
+
+if goodGrade == "I'm not doing very well"
+{
+    print("I should be getting an A at this point")
+}
